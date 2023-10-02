4559262efdb929be9fb4fc77dbfb71db7fd5ac77
TheAwiteb <awiteb@hotmail.com>
2023-10-02 13:01:47 +0300

    Init commit

---
+++ README.md
@@ -0,0 +1,24 @@
+## ARPStore SDK
+
+A SDK for ARPStore API.
+
+### Usage
+
+```rust
+use arpstore_sdk::Client;
+
+const PRODUCT_CODE: &str = "the_product_code";
+
+#[tokio::main]
+async fn main() {
+    let arp_client = Client::new("https://api.yourdomain.com", "your_api_key");
+    let result = arp_client.is_valid_subscription(PRODUCT_CODE).await;
+    match result {
+        Ok(_) => println!("Subscription is valid"),
+        Err(e) => println!("{e}"), // The error is a string from the API, or a reqwest error
+    }
+}
+```
+
+### License
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details
---
+++ src/lib.rs
@@ -0,0 +1,114 @@
+use machineid_rs::{Encryption, HWIDComponent, IdBuilder};
+use sha2::{Digest, Sha256};
+
+/// Errors from the ARPStore API.
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    /// An error from the reqwest crate.
+    #[error("HTTP error: {0}")]
+    Reqwest(#[from] reqwest::Error),
+
+    /// An error from the serde_json crate.
+    #[error("JSON error: {0}")]
+    SerdeJson(#[from] serde_json::Error),
+
+    /// An error from the ARP API.
+    #[error("Error: {0}")]
+    Api(String),
+}
+
+fn create_signature(subscription_key: &str, device_hash: &str, timestamp: String) -> String {
+    let mut hasher = Sha256::new();
+    hasher.update(subscription_key.as_bytes());
+    hasher.update(device_hash.as_bytes());
+    hasher.update(timestamp.as_bytes());
+    hasher
+        .finalize()
+        .iter()
+        .map(|b| format!("{:02x}", b))
+        .collect()
+}
+
+fn add_headers(
+    request: reqwest::RequestBuilder,
+    subscription_key: &str,
+) -> reqwest::RequestBuilder {
+    request
+        .header("X-API-KEY", subscription_key)
+        .header("Date", chrono::Utc::now().to_rfc2822())
+}
+
+fn add_body(
+    request: reqwest::RequestBuilder,
+    subscription_key: &str,
+    device_hash: &str,
+) -> reqwest::RequestBuilder {
+    let timestamp = chrono::Utc::now().timestamp();
+    let signature = create_signature(subscription_key, device_hash, timestamp.to_string());
+    request.json(&serde_json::json!(
+        {
+            "device_hash": device_hash,
+            "timestamp": timestamp,
+            "signature": signature
+        }
+    ))
+}
+
+/// A client for the ARPStore API.
+#[derive(Debug)]
+pub struct Client {
+    client: reqwest::Client,
+    subscription_key: String,
+    arp_url: String,
+}
+
+impl Client {
+    /// Create a new client for the ARPStore API.
+    pub fn new(arp_url: impl Into<String>, subscription_key: impl Into<String>) -> Self {
+        Self {
+            client: reqwest::Client::new(),
+            subscription_key: subscription_key.into(),
+            arp_url: arp_url.into(),
+        }
+    }
+
+    /// Check if a subscription is valid.
+    pub async fn is_valid_subscription(&self, product_code: &str) -> Result<(), Error> {
+        let device_hash = IdBuilder::new(Encryption::SHA256)
+            .add_component(HWIDComponent::CPUID)
+            .add_component(HWIDComponent::SystemID)
+            .add_component(HWIDComponent::DriveSerial)
+            .build(&self.subscription_key)
+            .expect("this should never fail, because we're not using machine name or os name");
+        let request_builder = add_body(
+            add_headers(
+                self.client
+                    .post(format!("{}/activation_check", self.arp_url)),
+                &self.subscription_key,
+            ),
+            &self.subscription_key,
+            &device_hash,
+        );
+        let response = request_builder.send().await?;
+        let status = response.status();
+        let message = response
+            .json::<serde_json::Value>()
+            .await?
+            .get("message")
+            .unwrap()
+            .to_string();
+        let message = message.trim_matches('"').to_owned();
+
+        if status == reqwest::StatusCode::OK {
+            if message.ends_with(product_code) {
+                Ok(())
+            } else {
+                Err(Error::Api(
+                    "The subscription is not for this product!".to_string(),
+                ))
+            }
+        } else {
+            Err(Error::Api(message))
+        }
+    }
+}
